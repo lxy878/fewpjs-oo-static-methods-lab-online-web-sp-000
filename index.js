425a230e311f1414f7430df7783c3939431c3c44
Xiaoyi Liu <lxy878@gmail.com>
2020-09-07 09:23:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class Formatter {
   }
 
   static titleize(s){
-    return s.split(' ').map(word => {
+    return s.split(' ').map((word, i) => {
       if (!(word === 'the' || word === 'a' || word === 'an' || word === 'but' || word === 'of' || word === 'and' || word === 'for' || word === 'at' || word === 'by' || word === 'from')){
         return this.capitalize(word)
       }else{
