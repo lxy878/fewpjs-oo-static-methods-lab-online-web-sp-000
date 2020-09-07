3dfd547466eb82dd65933bb93397440fd802fcda
Xiaoyi Liu <lxy878@gmail.com>
2020-09-07 09:24:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class Formatter {
 
   static titleize(s){
     return s.split(' ').map((word, i) => {
-      if (!(word === 'the' || word === 'a' || word === 'an' || word === 'but' || word === 'of' || word === 'and' || word === 'for' || word === 'at' || word === 'by' || word === 'from')){
+      if (!(word === 'the' || word === 'a' || word === 'an' || word === 'but' || word === 'of' || word === 'and' || word === 'for' || word === 'at' || word === 'by' || word === 'from') && i > 0){
         return this.capitalize(word)
       }else{
         return word
