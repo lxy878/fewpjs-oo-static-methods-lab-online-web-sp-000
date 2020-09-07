e4acb66926106a29cd5624261b26cd136e520c95
Xiaoyi Liu <lxy878@gmail.com>
2020-09-07 09:23:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ class Formatter {
   static titleize(s){
     return s.split(' ').map(word => {
       if (!(word === 'the' || word === 'a' || word === 'an' || word === 'but' || word === 'of' || word === 'and' || word === 'for' || word === 'at' || word === 'by' || word === 'from')){
-        this.capitalize(word)
+        return this.capitalize(word)
       }else{
-        word
+        return word
       }
 
     }).join(' ');
