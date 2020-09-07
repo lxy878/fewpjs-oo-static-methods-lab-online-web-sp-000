44941dfb5eef38c202200f5beb43c250a9c1b9ea
Xiaoyi Liu <lxy878@gmail.com>
2020-09-07 09:22:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class Formatter {
 
   static titleize(s){
     return s.split(' ').map(word => {
-      if (word === 'the' || word === 'a' || word === 'an' || word === 'but' || word === 'of' || word === 'and' || word === 'for' || word === 'at' || word === 'by' || word === 'from'){
+      if (!(word === 'the' || word === 'a' || word === 'an' || word === 'but' || word === 'of' || word === 'and' || word === 'for' || word === 'at' || word === 'by' || word === 'from')){
         this.capitalize(word)
       }else{
         word
